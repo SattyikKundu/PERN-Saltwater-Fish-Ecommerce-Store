ef0195fb03790886d67b756dd18681e618e93b5a
Sattyik Kundu <sattyikkundu@gmail.com>
2026-09-17 18:17:15 -0400

    Removed excess comments from database.js in '/server' folder.

--- server/database/database.js
+++ server/database/database.js
@@ -6,24 +6,10 @@ import dotenv from 'dotenv'; // loads .env variables into process.env
                              // so they can be accessed anywhere in server code
 dotenv.config();
 
-// Created pool of database connections for PostgreSQL (later move this in an .env file, like in realworld App)
-// NOTE: This below pool is commented out since this only works for the PostgreSQL on my localhost machine.
-//       The next 'pool' is used in a *production* setting where web app on Render.com accesses the database
-//       store in Neon.com.
-
-/*
-const pool = new Pool({
-    user:     process.env.DB_USER,        // your postgre database's username
-    host:     process.env.DB_HOST,        // since it works on my local computer
-    database: process.env.DB_NAME,        // name of database
-    password: process.env.DB_PASSWORD,    // password to access database
-    port:     process.env.DB_PORT         // port where postgreSQL database is accessible 
-});*/
-
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
   ssl: {
-    rejectUnauthorized: false  // Required for Neon on Render
+    rejectUnauthorized: false  
   }
 });
 
